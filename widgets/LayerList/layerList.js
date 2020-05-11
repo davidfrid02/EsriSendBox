06285b77b253860fb8d337b51b1586f6e1140ac0
davidfrid02 <frid.david@gmail.com>
2020-05-11 11:18:46 +0300

    finish symbology without style

--- widgets/LayerList/layerList.js
+++ widgets/LayerList/layerList.js
@@ -17,10 +17,9 @@ define(["esri/widgets/Expand", "esri/widgets/LayerList"], (
   };
 
   const defineActions = (event) => {
-    var item = event.item;
+    const item = event.item;
     const divPanel = document.createElement("div");
-    divPanel.layerId = item.layer.id;
-    createSymbolSection(divPanel, item.layer.renderer);
+    createSymbolSection(divPanel, item.layer);
     createLabelSection(divPanel, item.layer);
 
     item.panel = {
@@ -29,22 +28,46 @@ define(["esri/widgets/Expand", "esri/widgets/LayerList"], (
   };
 
   // ===== SYMBOL ======
-  const createSymbolSection = (element, renderer) => {
-    let color = "blue";
+  const createSymbolSection = (element, layer) => {
+    let color = "#0000ff";
     let size = 14;
-    let style = "rectangle";
+    let style = "circle";
     let angle = 0;
-    let outlineColor = "white";
+    let outlineColor = "#000000";
     let outlineWidth = 1;
     let outlineStyle = "solid";
+    let outlineStyleOptions = [
+      "dash",
+      "dash-dot",
+      "dot",
+      "inside-frame",
+      "long-dash",
+      "long-dash-dot",
+      "long-dash-dot-dot",
+      "none",
+      "short-dash",
+      "short-dash-dot",
+      "short-dash-dot-dot",
+      "short-dot",
+      "solid",
+    ];
+    let styleOptions = [
+      "circle",
+      "cross",
+      "diamond",
+      "square",
+      "triangle",
+      "x",
+    ];
+    let renderer = layer.renderer;
     if (renderer && renderer.symbol) {
-      color = renderer.symbol.color;
+      color = rgbToHex(renderer.symbol.color);
       size = renderer.symbol.size;
       style = renderer.symbol.style;
       angle = renderer.symbol.angle;
       //OUTLINE
       if (renderer.symbol.outline) {
-        outlineColor = renderer.symbol.outline.color;
+        outlineColor = rgbToHex(renderer.symbol.outline.color);
         outlineWidth = renderer.symbol.outline.width;
         outlineStyle = renderer.symbol.outline.style;
       }
@@ -54,94 +77,98 @@ define(["esri/widgets/Expand", "esri/widgets/LayerList"], (
     header.innerText = "======= Symbology! =======";
     element.appendChild(header);
 
-    createColorPicker(element, color, "symbol", "color");
-    createRange(element, "symbol", "size", "sizeSliderValue", {
-      value: size,
-      min: 1,
-      max: 100,
-    });
-    createSelect(
+    createColorPicker(element, color, "symbol", "color", layer);
+    createRange(
       element,
-      ["circle", "cross", "diamond", "square", "triangle", "x"],
       "symbol",
-      "style"
+      "size",
+      "sizeSliderValue",
+      {
+        value: size,
+        min: 1,
+        max: 100,
+      },
+      layer
     );
-    createRange(element, "symbol", "angle", "angleSliderValue", {
-      value: angle,
-      min: -180,
-      max: 180,
-    });
+    createSelect(element, style, styleOptions, "symbol", "style", layer);
+    createRange(
+      element,
+      "symbol",
+      "angle",
+      "angleSliderValue",
+      {
+        value: angle,
+        min: -180,
+        max: 180,
+      },
+      layer
+    );
 
     //==========OUTLINE=========
-    createColorPicker(element, outlineColor, "symbol", "outline-color");
-    createRange(element, "symbol", "outline-width", "outlineWidthSliderValue", {
-      value: angle,
-      min: 1,
-      max: 20,
-    });
+    createColorPicker(element, outlineColor, "symbol", "outline-color", layer);
+    createRange(
+      element,
+      "symbol",
+      "outline-width",
+      "outlineWidthSliderValue",
+      {
+        value: outlineWidth,
+        min: 1,
+        max: 20,
+      },
+      layer
+    );
     createSelect(
       element,
-      [
-        "dash",
-        "dash-dot",
-        "dot",
-        "inside-frame",
-        "long-dash",
-        "long-dash-dot",
-        "long-dash-dot-dot",
-        "none",
-        "short-dash",
-        "short-dash-dot",
-        "short-dash-dot-dot",
-        "short-dot",
-        "solid",
-      ],
-      "outline",
-      "style"
+      outlineStyle,
+      outlineStyleOptions,
+      "symbol",
+      "outline-style",
+      layer
     );
   };
 
-  const changeSymbol = (event, attribute) => {
-    const layerId = event.currentTarget.parentElement.layerId;
-    let layer = view.map.findLayerById(layerId);
+  const changeSymbol = (event, attribute, layer) => {
     if (layer.renderer) {
       let currRenderer = layer.renderer.clone();
-      let value = event.target.value;
-      switch (attribute) {
-        case "color":
-          currRenderer.symbol.color = value;
-          break;
-        case "size":
-          event.currentTarget.parentElement.getElementsByClassName(
-            "sizeSliderValue"
-          )[0].innerText = value;
-          currRenderer.symbol.size = value;
-          break;
-        case "style":
-          currRenderer.symbol.style = event.target.selectedOptions[0].value;
-          break;
-        case "angle":
-          event.currentTarget.parentElement.getElementsByClassName(
-            "angleSliderValue"
-          )[0].innerText = value;
-          currRenderer.symbol.angle = value;
-          break;
-        //======OUTLINE======
-        case "outline-color":
-          currRenderer.symbol.outline.color = value;
-          break;
-        case "outline-width":
-          event.currentTarget.parentElement.getElementsByClassName(
-            "outlineWidthSliderValue"
-          )[0].innerText = value;
-          currRenderer.symbol.outline.width = value;
-          break;
-        case "outline-style":
-          currRenderer.symbol.outline.style =
-            event.target.selectedOptions[0].value;
-          break;
+      if (currRenderer.type !== "unique-value") {
+        let value = event.target.value;
+        switch (attribute) {
+          case "color":
+            currRenderer.symbol.color = value;
+            break;
+          case "size":
+            event.currentTarget.parentElement.getElementsByClassName(
+              "sizeSliderValue"
+            )[0].innerText = value;
+            currRenderer.symbol.size = value;
+            break;
+          case "style":
+            currRenderer.symbol.style = event.target.selectedOptions[0].value;
+            break;
+          case "angle":
+            event.currentTarget.parentElement.getElementsByClassName(
+              "angleSliderValue"
+            )[0].innerText = value;
+            currRenderer.symbol.angle = value;
+            break;
+          //======OUTLINE======
+          case "outline-color":
+            currRenderer.symbol.outline.color = value;
+            break;
+          case "outline-width":
+            event.currentTarget.parentElement.getElementsByClassName(
+              "outlineWidthSliderValue"
+            )[0].innerText = value;
+            currRenderer.symbol.outline.width = value;
+            break;
+          case "outline-style":
+            currRenderer.symbol.outline.style =
+              event.target.selectedOptions[0].value;
+            break;
+        }
+        layer.renderer = currRenderer;
       }
-      layer.renderer = currRenderer;
     }
   };
 
@@ -152,74 +179,109 @@ define(["esri/widgets/Expand", "esri/widgets/LayerList"], (
       let fields = layer.fields.map((field) => field.name);
       fields.unshift("");
       let field = fields[0];
-      let color,
-        haloColor = "blue";
-      let size = 14;
+      let color = "#0000ff";
+      let haloColor = "#00ff00";
       let haloSize = 1;
-      // let style = "rectangle";
-      // let angle = 0;
-      // let outlineColor = "white";
-      // let outlineWidth = 1;
-      // let outlineStyle = "solid";
+      let fontFamily = "Arial Unicode MS";
+      let fontSize = "12";
+      let labelPlacement = "above-right";
+      let decoration = "none";
+      let weight = "normal";
+      let italic = "normal";
+
+      let fontFamilyOptions = [
+        "Arial Unicode MS",
+        "Ubuntu Mono",
+        "Sans-Serif",
+        "Josefin Slab",
+        "Merriweather",
+        "Fantasy",
+        "Cursive",
+      ];
+      let fontSizeOptions = [
+        "8",
+        "10",
+        "12",
+        "14",
+        "16",
+        "18",
+        "20",
+        "25",
+        "30",
+        "35",
+        "40",
+      ];
+      let haloSizeOptions = [...Array(10).keys()];
+
       if (labelingInfo && labelingInfo.length) {
-        field = layer.labelingInfo[0].labelExpressionInfo.split(".")[1];
-        color = labelingInfo.symbol.color;
-        size = renderer.labelingInfo.size;
-        haloColor = renderer.labelingInfo.haloColor;
-        haloSize = renderer.labelingInfo.haloSize;
-        // style = renderer.labelingInfo.style;
-        // angle = renderer.labelingInfo.angle;
-        // //OUTLINE
-        // if (renderer.labelingInfo.outline) {
-        //   outlineColor = renderer.symbol.outline.color;
-        //   outlineWidth = renderer.symbol.outline.width;
-        //   outlineStyle = renderer.symbol.outline.style;
-        // }
+        let labelClass = layer.labelingInfo[0];
+        field = labelClass.labelExpressionInfo.split(".")[1];
+        labelPlacement = labelClass.labelPlacement;
+        color = rgbToHex(labelClass.symbol.color);
+        haloColor = rgbToHex(labelClass.symbol.haloColor);
+        haloSize = labelClass.symbol.haloSize;
+        fontFamily = labelClass.symbol.font.family;
+        fontSize = labelClass.symbol.font.size;
+        decoration = labelClass.symbol.font.decoration;
+        weight = labelClass.symbol.font.weight;
+        italic = labelClass.symbol.font.italic;
       }
 
       let header = document.createElement("p");
       header.innerText = "======= Label! =======";
       element.appendChild(header);
 
-      createSelect(element, fields, "label", "field");
-      createColorPicker(element, color, "label", "color");
+      createSelect(element, field, fields, "label", "field", layer);
+      createColorPicker(element, color, "label", "color", layer);
       createSelect(
         element,
-        [
-          "serif",
-          "sans-serif",
-          "Josefin Slab",
-          "Merriweather",
-          "fantasy",
-          "cursive",
-        ],
+        fontFamily,
+        fontFamilyOptions,
         "label",
-        "font-family"
+        "font-family",
+        layer
       );
       createSelect(
         element,
-        ["8", "10", "12", "14", "16", "18", "20", "25", "30", "35", "40"],
+        fontSize,
+        fontSizeOptions,
         "label",
-        "font-size"
+        "font-size",
+        layer
       );
 
-      createButton(element, "U", "U", "label", "font-decoration");
-      createButton(element, "B", "B", "label", "font-weight");
-      createButton(element, "I", "I", "label", "font-style");
-      createColorPicker(element, haloColor, "label", "haloColor");
-      createSelect(element, [...Array(10).keys()], "label", "haloSize");
+      createButton(element, "U", "U", "label", "font-decoration", layer);
+      createButton(element, "B", "B", "label", "font-weight", layer);
+      createButton(element, "I", "I", "label", "font-style", layer);
+      createColorPicker(element, haloColor, "label", "haloColor", layer);
+      createSelect(
+        element,
+        haloSize,
+        haloSizeOptions,
+        "label",
+        "haloSize",
+        layer
+      );
 
-      createButton(element, "<", "top-left", "label", "alignment");
+      createButton(element, "<", "above-left", "label", "placement", layer);
+      createButton(element, "|", "above-center", "label", "placement", layer);
+      createButton(element, ">", "above-right", "label", "placement", layer);
+
+      createButton(element, "<<", "center-left", "label", "placement", layer);
+      createButton(element, "||", "center-center", "label", "placement", layer);
+      createButton(element, ">>", "center-right", "label", "placement", layer);
+
+      createButton(element, "<<<", "below-left", "label", "placement", layer);
+      createButton(element, "|||", "below-center", "label", "placement", layer);
+      createButton(element, ">>>", "below-right", "label", "placement", layer);
     }
   };
 
-  const changeLabel = (event, attribute) => {
-    const layerId = event.currentTarget.parentElement.layerId;
-    let layer = view.map.findLayerById(layerId);
+  const changeLabel = (event, attribute, layer) => {
     let value = event.target.value;
     let labelClass;
     if (layer.labelingInfo) {
-      labelClass = layer.labelingInfo[0];
+      labelClass = layer.labelingInfo[0].clone();
     } else {
       labelClass = {
         symbol: {
@@ -229,6 +291,7 @@ define(["esri/widgets/Expand", "esri/widgets/LayerList"], (
         labelExpressionInfo: {
           expression: "$feature.SymbolId",
         },
+        labelPlacement: "above-center",
       };
     }
     switch (attribute) {
@@ -247,15 +310,6 @@ define(["esri/widgets/Expand", "esri/widgets/LayerList"], (
         break;
       case "haloSize":
         labelClass.symbol.haloSize = event.target.selectedOptions[0].value;
-      case "alignment":
-        //need to finish! not working good!
-        switch (value) {
-          case "top-left":
-            labelClass.symbol.horizontalAlignment = "left";
-            labelClass.symbol.verticalAlignment = "top";
-            break;
-        }
-        break;
       // ==== FONT ====
       case "font-family":
         labelClass.symbol.font.family = event.target.selectedOptions[0].value;
@@ -275,6 +329,10 @@ define(["esri/widgets/Expand", "esri/widgets/LayerList"], (
         labelClass.symbol.font.weight =
           labelClass.symbol.font.weight == "normal" ? "bold" : "normal";
         break;
+      // ==== labelPlacement =====
+      case "placement":
+        labelClass.labelPlacement = value;
+        break;
     }
     layer.labelingInfo = [labelClass];
   };
@@ -282,22 +340,22 @@ define(["esri/widgets/Expand", "esri/widgets/LayerList"], (
   // ==================================
   // ======= creator functions ========
   // ==================================
-  const createButton = (element, text, value, type, attribute) => {
+  const createButton = (element, text, value, type, attribute, layer) => {
     const button = document.createElement("button");
     button.innerHTML = text;
     button.value = value;
     button.onclick = (event) => {
-      callbackFunction(event, type, attribute);
+      callbackFunction(event, type, attribute, layer);
     };
     element.appendChild(button);
   };
 
-  const createColorPicker = (element, value, type, attribute) => {
+  const createColorPicker = (element, defaultValue, type, attribute, layer) => {
     const colorPicker = document.createElement("input");
     colorPicker.type = "color";
-    colorPicker.value = value;
+    colorPicker.value = defaultValue;
     colorPicker.onchange = (event) => {
-      callbackFunction(event, type, attribute);
+      callbackFunction(event, type, attribute, layer);
     };
     element.appendChild(colorPicker);
   };
@@ -307,7 +365,8 @@ define(["esri/widgets/Expand", "esri/widgets/LayerList"], (
     type,
     attribute,
     valueElement,
-    { value, min, max }
+    { value, min, max },
+    layer
   ) => {
     const range = document.createElement("input");
     range.type = "range";
@@ -315,7 +374,7 @@ define(["esri/widgets/Expand", "esri/widgets/LayerList"], (
     range.min = min;
     range.max = max;
     range.onchange = (event) => {
-      callbackFunction(event, type, attribute);
+      callbackFunction(event, type, attribute, layer);
     };
     const rangeValue = document.createElement("p");
     rangeValue.innerText = value;
@@ -325,30 +384,57 @@ define(["esri/widgets/Expand", "esri/widgets/LayerList"], (
     element.appendChild(rangeValue);
   };
 
-  const createSelect = (element, optionsArray, type, attribute) => {
+  const createSelect = (
+    element,
+    defaultValue,
+    optionsArray,
+    type,
+    attribute,
+    layer
+  ) => {
     const select = document.createElement("select");
     for (option of optionsArray) {
-      createOption(select, option);
+      if (option == defaultValue) {
+        createOption(select, option, true);
+      } else {
+        createOption(select, option, false);
+      }
     }
     select.onchange = (event) => {
-      callbackFunction(event, type, attribute);
+      callbackFunction(event, type, attribute, layer);
     };
     element.appendChild(select);
   };
 
-  const callbackFunction = (event, type, attribute) => {
+  const callbackFunction = (event, type, attribute, layer) => {
     if (type == "symbol") {
-      changeSymbol(event, attribute);
+      changeSymbol(event, attribute, layer);
     } else if (type == "label") {
-      changeLabel(event, attribute);
+      changeLabel(event, attribute, layer);
     }
   };
 
-  const createOption = (selectElement, value) => {
+  const createOption = (selectElement, value, selected) => {
     const option = document.createElement("option");
     option.value = value;
     option.innerText = value;
+    if (selected) {
+      option.selected = selected;
+    }
     selectElement.appendChild(option);
   };
+
+  const rgbToHex = ({ r, g, b }) => {
+    return (
+      "#" +
+      [r, g, b]
+        .map((x) => {
+          const hex = x.toString(16);
+          return hex.length === 1 ? "0" + hex : hex;
+        })
+        .join("")
+    );
+  };
+
   return { load };
 });
